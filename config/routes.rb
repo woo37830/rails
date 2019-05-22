f3ca6acdd095f075195156ea240419cb7c2d7246
John Wooten <jwooten3730@me.com>
2019-05-22 17:48:50 -0400

    Added About view

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index]
   resources :comments
   resources :posts
+  get "/:page" => "static#show"
   root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
